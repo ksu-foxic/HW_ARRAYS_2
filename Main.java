e171b4942edc9a102cbd04b32308ba00fc1f70ca
ksu-foxic <kseniamitina1985@gmail.com>
2025-06-30 08:49:44 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -6,41 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] colors = new int[SIZE][SIZE];
+        int[][] matrix = new int[SIZE][SIZE];
 
         Random random = new Random();
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                colors[i][j] = random.nextInt(256);
+                matrix[i][j] = random.nextInt(256);
             }
         }
 
-        printColors(colors);
-        printRotate(colors);
+        printMatrix(matrix);
+        printRotate(matrix);
     }
 
-    public static void printColors(int[][] colors) {
+    public static void printMatrix(int[][] cell) {
         System.out.println("Дана следующая матрица:");
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", colors[i][j]);
+                System.out.format("%4d", cell [i][j]);
             }
             System.out.println();
         }
     }
 
-    public static void printRotate(int[][] colors) {
+    public static void printRotate(int[][] cell) {
         System.out.println("Повернутая на 90 градусов матрица:");
-        int[][] rotatedColors = new int[SIZE][SIZE];
+        int[][] rotatedMatrix = new int[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                rotatedColors[j][SIZE - 1 - i] = colors[i][j];
+                rotatedMatrix[j][SIZE - 1 - i] = cell [i][j];
             }
         }
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", rotatedColors[i][j]);
+                System.out.format("%4d", rotatedMatrix[i][j]);
             }
             System.out.println();
         }
