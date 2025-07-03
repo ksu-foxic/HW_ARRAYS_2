d2f947c29b27820fc0b6991462a2e06ed0c12ed9
ksu-foxic <kseniamitina1985@gmail.com>
2025-07-03 10:12:57 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -6,43 +6,46 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] matrix = new int[SIZE][SIZE];
+        int[][] matrix = createMatrix(SIZE, SIZE);//  Создание матрицы
 
+        System.out.println("Дана следующая матрица:");//  Вывод матрицы до поворота
+        printMatrix(matrix);
+
+        int[][] rotateMatriix = rotateMatrix(matrix);//  Разворот матрицы
+
+        System.out.println();
+        System.out.println("Повернутая на 90 градусов матрица:");//  Вывод матрицы после поворота
+        printMatrix(rotateMatriix);
+    }
+
+    public static int[][] rotateMatrix(int[][] matrix) {
+        int[][] rotatedMatrix = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                rotatedMatrix[j][SIZE - 1 - i] = matrix[i][j];
+            }
+        }
+        return rotatedMatrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.format("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] createMatrix(int row, int column) {
+        int[][] matrix = new int[row][column];
         Random random = new Random();
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 matrix[i][j] = random.nextInt(256);
             }
         }
-
-        printMatrix(matrix);
-        printRotate(matrix);
+        return matrix;
     }
 
-    public static void printMatrix(int[][] cell) {
-        System.out.println("Дана следующая матрица:");
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", cell [i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printRotate(int[][] cell) {
-        System.out.println("Повернутая на 90 градусов матрица:");
-        int[][] rotatedMatrix = new int[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                rotatedMatrix[j][SIZE - 1 - i] = cell [i][j];
-            }
-        }
-
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", rotatedMatrix[i][j]);
-            }
-            System.out.println();
-        }
-    }
 }
